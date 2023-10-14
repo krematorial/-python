2a174d4858d81fc6f4b34130ae5a1d0c11623a66
krematorial <137116006+krematorial@users.noreply.github.com>
2023-10-14 16:18:42 +0300

    Add files via upload

---
+++ Лабораторная работа 1/task_2.py
@@ -0,0 +1,6 @@
+list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
+
+# TODO Разделите участников на две команды
+length = len(list_players)//2
+print(list_players[:length])
+print(list_players[length:])
